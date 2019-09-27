b13cd69732f8b8dbb5461fda8e5b7be11588cb6e
Ivan Ali Rodriguez Vasquez <ali.rodri.vasquez@gmail.com>
2019-09-27 12:07:48 -0500

    Crear metodos y test
    Se crean los metodos del controlador y las pruebas correspondientes de los metodos.

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,5 +1,48 @@
 class CompaniesController < ApplicationController
+  before_action :set_company, only: %w[show, edit, update, destroy]
+
   def index
     @company = Company.first
+  end
+
+  def show; end
+
+  def new
+    @company = Company.new
+  end
+
+  def edit; end
+
+  def create
+    @company = Company.new(company_params)
+    if @company.save
+      flash[:success] = ""
+      redirect_to companies_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @company.update(company_params)
+      flash[:success] = ""
+      redirect_to companies_path
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    @company.destroy
+    flash[:success] = ""
+  end
+
+  private
+  def company_params
+    params.require(:company).permit(:name, :website)
+  end
+
+  def set_company
+    @company = Company.find(params[:id])
   end
 end
--- spec/controllers/companies_controller_spec.rb
+++ spec/controllers/companies_controller_spec.rb
@@ -1,6 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe CompaniesController, type: :controller do
+  before(:each) do
+    @company = create(:company)
+  end
+
+  it { should route(:get, '/companies').to(action: :index) }
+  it { should route(:get, '/companies/1').to(action: :show, id: 1) }
+  it { should route(:get, '/companies/new').to(action: :new) }
+  it { should route(:get, '/companies/1/edit').to(action: :edit, id: 1) }
+  it { should route(:post, '/companies').to(action: :create) }
+  it { should route(:put, '/companies/1').to(action: :update, id:1) }
+  it { should route(:delete, '/companies/1').to(action: :destroy, id: 1) }
 
   describe "GET #index" do
     it "returns http success" do

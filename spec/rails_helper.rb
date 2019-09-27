66b60aba9cc1b0bb7c4122b9ffcaa0fa83543f2e
Ivan Ali Rodriguez Vasquez <ali.rodri.vasquez@gmail.com>
2019-09-27 02:42:34 -0500

    Agregar support
    Se añade support de las diferentes gemas.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,11 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'support/factory_bot'
+require 'support/database_cleaner'
+require 'support/shoulda_matchers'
+
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 begin
   ActiveRecord::Migration.maintain_test_schema!
@@ -18,11 +23,4 @@ RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
-end
-
-Shoulda::Matchers.configure do |config|
-  config.integrate do |with|
-    with.test_framework :rspec
-    with.library :rails
-  end
 end

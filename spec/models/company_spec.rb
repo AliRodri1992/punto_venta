3dd18dfd3f142d2b96a601ee924dcd9f0260c136
Ivan Ali Rodriguez Vasquez <ali.rodri.vasquez@gmail.com>
2019-09-27 10:47:02 -0500

    Agregar nuevos test
    Se añade dos nuevos test en el modelo de company

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,7 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Company, type: :model do
-  describe 'Validations' do
+  context 'validations' do
     it { should validate_presence_of :name }
+  end
+
+  it 'is valid' do
+    company = create(:company)
+    expect(company).to be_valid
   end
 end

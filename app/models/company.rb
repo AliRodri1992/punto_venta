a8ff7321ddf5ceea8e54ce788020443b21cd8ea8
Ivan Ali Rodriguez Vasquez <ali.rodri.vasquez@gmail.com>
2019-10-03 10:23:49 -0500

    Validaciones del formulario
    Se valida los campos que deben ser obligatorios en el formulario de empresas.

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,6 +2,11 @@ class Company < ApplicationRecord
   has_one_attached :logo
 
   validates :name, presence: true
+  validates :name, length: { minimum: 3 }, allow_blank: true
+  validates :website, presence: true
+  validates :website, length: { minimum: 3 }, allow_blank: true
+
+  validates_format_of :name, with: /^([a-z A-ZñáéíóúÑÁÉÍÓÚ[0-9]\s]+)$/, multiline: true, allow_blank: true
 
   def thumbnail
     return self.logo.variant(resize: "300x300")
